ecd00fd3f8a25b1802292f0a903d7c6c48ba09a2
Sergio Vilar <sergiovilar.r@gmail.com>
2015-09-10 15:41:25 -0300

    Site updated at 2015-09-10 18:41:25 UTC

--- assets/js/search.js
+++ assets/js/search.js
@@ -35,35 +35,3 @@ window.App.controller('SearchController', function($scope) {
   });
 
 });
-
-
-jQuery(function() {
-
-
-  function display_search_results(results) {
-    var $search_results = $("#search_results");
-
-    // Wait for data to load
-    window.data.then(function(loaded_data) {
-
-      // Are there any results?
-      if (results.length) {
-        $search_results.empty(); // Clear any old results
-
-        // Iterate over the results
-        results.forEach(function(result) {
-          var item = loaded_data[result.ref];
-          console.log(item);
-
-          // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
-
-          // Add it to the results
-          $search_results.append(appendString);
-        });
-      } else {
-        $search_results.html('<li>No results found</li>');
-      }
-    });
-  }
-});
